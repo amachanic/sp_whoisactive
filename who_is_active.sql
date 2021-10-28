c9602475567c70ebb26323c81fab9271fb0afae3
Michael Fuller <86622523+mfuller333@users.noreply.github.com>
2021-10-28 19:38:18 -0400

    Add files via upload
    LEFT JOIN to LEFT OUTER JOIN

--- who_is_active.sql
+++ who_is_active.sql
@@ -3170,19 +3170,19 @@ BEGIN;
 						'+
 					CASE
 						WHEN (@get_memory_grant_info = 1 AND @sql_version >= 1001600) THEN
-							'LEFT JOIN sys.dm_exec_query_stats AS qs ON
+							'LEFT OUTER JOIN sys.dm_exec_query_stats AS qs ON
 									r.sql_handle = qs.sql_handle
 								AND	r.plan_handle = qs.plan_handle
 								AND	r.statement_start_offset = qs.statement_start_offset
-							LEFT JOIN sys.dm_exec_query_memory_grants mg ON
+							LEFT OUTER JOIN sys.dm_exec_query_memory_grants mg ON
 									mg.session_id = sp.session_id 
 								AND	mg.request_id = sp.request_id
-							LEFT JOIN sys.dm_exec_query_resource_semaphores rs ON
+							LEFT OUTER JOIN sys.dm_exec_query_resource_semaphores rs ON
 									mg.resource_semaphore_id = rs.resource_semaphore_id 
 								AND	mg.pool_id = rs.pool_id
-							LEFT JOIN sys.resource_governor_workload_groups wg ON
+							LEFT OUTER JOIN sys.resource_governor_workload_groups wg ON
 									s.group_id = wg.group_id
-							LEFT JOIN sys.resource_governor_resource_pools rp ON
+							LEFT OUTER JOIN sys.resource_governor_resource_pools rp ON
 									wg.pool_id = rp.pool_id'
 						ELSE
 							''
